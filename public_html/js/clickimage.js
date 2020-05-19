fa42d86fb6fd7dd074551cf17a651d1da1196b08
blacherelogan <blacherelogan@gmail.com>
2020-05-20 00:36:11 +0200

    page bonus faite + video plus qu'as mettre un apercus (image pour click et afficher sur carousel)

--- public_html/js/clickimage.js
+++ public_html/js/clickimage.js
@@ -13,3 +13,11 @@ function la3() {
     document.getElementById("im4").onclick = function() {la4()};    
 function la4() {
     $("#carouselExampleIndicators").carousel(3);}         
+
+    document.getElementById("im5").onclick = function() {la5()};    
+function la5() {
+    $("#carouselExampleIndicators").carousel(4);}    
+
+    document.getElementById("im6").onclick = function() {la6()};    
+function la6() {
+    $("#carouselExampleIndicators").carousel(5);}    
