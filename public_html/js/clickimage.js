15ac059ce784789c69bc73738d66fa3871541084
blacherelogan <blacherelogan@gmail.com>
2020-05-19 11:28:54 +0200

    script du déplacment d'image mit dans un .js a part (fonctionnel)

---
+++ public_html/js/clickimage.js
@@ -0,0 +1,15 @@
+document.getElementById("im1").onclick = function() {la1()};
+function la1() {
+    $("#carouselExampleIndicators").carousel(0);} 
+    
+    document.getElementById("im2").onclick = function() {la2()};
+function la2() {
+    $("#carouselExampleIndicators").carousel(1);}    
+    
+    document.getElementById("im3").onclick = function() {la3()};
+function la3() {
+    $("#carouselExampleIndicators").carousel(2);}
+            
+    document.getElementById("im4").onclick = function() {la4()};    
+function la4() {
+    $("#carouselExampleIndicators").carousel(3);}         
